e63cf24042a3d197a614d1d0e969d95a22d1abcd
dingna <dingna@datarc.cn>
2024-04-10 10:17:10 +0800

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
-export { default as GradientColorPicker, LinearGradient } from "./Gradient";
+export { default as GradientColorPicker } from "./Gradient";
 export { default as ColorsPicker } from "./Colors";
+export { default as ColorPicker } from "./Base";
 export { Color } from "@rc-component/color-picker";
